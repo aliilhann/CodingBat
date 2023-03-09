e89a1a225c1f3259390f58c071a92b38bc6b9a69
aliilhann <alirizailhan89@gmail.com>
2023-03-09 00:12:12 -0600

    first commit

---
+++ src/Array2/HaveThree.java
@@ -0,0 +1,27 @@
+package Array2;
+
+public class HaveThree {
+    public static void main(String[] args) {
+        int [] array={};
+        System.out.println(haveThree(array));
+        System.out.println("hola");
+
+    }
+    public static boolean haveThree(int[] nums) {
+        if(nums.length==0)
+            return false;
+        int sum=0;
+        for(int i=0; i<nums.length-1;i++) {
+            if(nums[i]==3 && nums[i+1]==3) {
+                return false;
+            }
+            if(nums[i]==3) {
+                sum++;
+            }
+        }
+        if(nums[nums.length-1]==3)
+            sum++;
+        return sum==3;
+    }
+}
+
